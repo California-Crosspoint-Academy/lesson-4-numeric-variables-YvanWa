c09684de5839db34270e770cbca6df7dd0e75893
YvanWa <yvanwang26@crosspointacademy.org>
2025-09-03 09:09:01 -0700

    commit

---
+++ Tester.java
@@ -0,0 +1,12 @@
+public class Tester {
+    public static void main(String arg[]){
+        int asnw = 79 + 3 * (4 + 82 - 68) - 7  + 19;
+        System.out.println("79 * 3 * (4 + 82 - 68) - 7  + 19 = " + asnw);
+
+        asnw = (179 + 21 + 10) / 7 + 181;
+        System.out.println("(179 + 21 + 10) / 7 + 181 = " + asnw);
+
+        asnw = 10389 * 56 * 11 + 2246;
+        System.out.println("10389 * 56 * 11 + 2246 = " + asnw);
+    }
+}
